05a5ed9b08b3d6f86c0e149b93cd38fecc2cb39f
hoonee02 <hoonee02@naver.com>
2023-05-01 14:40:29 +0900

    feat: Adding repeat Function

--- selector.py
+++ selector.py
@@ -1,4 +1,12 @@
 import random
 
-result = random.sample(range(1,45+1), k=6)
-print(result)
+T = int(input("how many times?: "))
+
+
+def get_lucky_nums():
+    result = random.sample(range(1,45+1), k=6)
+    print(result)
+
+if __name__ == '__main__':
+    for _ in range(T):
+        get_lucky_nums()
